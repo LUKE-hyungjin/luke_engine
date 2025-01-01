b07103582d477ab895fab1a576370f400fb9af65
Luke <e93052080@gmail.com>
2025-01-01 23:33:49 +0900

    finish Draw Triangle

--- luke_engine_source/Renderer.h
+++ luke_engine_source/Renderer.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "Camera.h"
+#include "GraphicDevice_DX11.h"
 
 namespace luke::renderer
 {
 	extern Camera* mainCamera;
+	struct Vertex
+	{
+		Vector3 pos;
+		Vector4 color;
+	};
+
+	extern Vertex vertexes[3];
+	extern ID3D11Buffer* vertexBuffer;
+	extern ID3DBlob* vsBlob;
+	extern ID3D11VertexShader* vsShader;
+	extern ID3DBlob* psBlob;
+	extern ID3D11PixelShader* psShader;
+	extern ID3D11InputLayout* inputLayouts;
 }
